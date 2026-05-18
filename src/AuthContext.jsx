00c7bc8c4299cf5f287dea9a5b26048af9fb5130
AvsaStudio <salome.a.miller@gmail.com>
2026-05-18 13:50:24 -0400

    added sessionStorage property

--- src/AuthContext.jsx
+++ src/AuthContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 const API = "https://fsa-jwt-practice.herokuapp.com";
 
@@ -8,11 +8,65 @@ export function AuthProvider({ children }) {
   const [token, setToken] = useState();
   const [location, setLocation] = useState("GATE");
 
+  useEffect(() => {
+    const storedToken = sessionStorage.getItem("token");
+
+    if (storedToken) {
+      setToken(storedToken);
+      setLocation("TABLET");
+    }
+  }, []);
+
   // TODO: signup
+  async function signup(username) {
+    const response = await fetch(`${API}/signup`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username,
+        password: "super-secret-999",
+      }),
+    });
+
+    const result = await response.json();
+
+    if (!response.ok || !result.success) {
+      throw Error(result.message);
+    }
+
+    setToken(result.token);
+    sessionStorage.setItem("token", result.token);
+    setLocation("TABLET");
+
+    return result;
+  }
 
   // TODO: authenticate
+  async function authenticate() {
+    if (!token) {
+      throw Error("No token found");
+    }
 
-  const value = { location };
+    const response = await fetch(`${API}/authenticate`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    const result = await response.json();
+    if (!response.ok || !result.success) {
+      throw Error(result.message);
+    }
+
+    setLocation("TUNNEL");
+  }
+
+  const value = { location, signup, authenticate };
+
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
 
